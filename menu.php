b0184385782b660c44a54f39dc4ee41554e01840
nfuegopy <93525722+nfuegopy@users.noreply.github.com>
2023-03-11 17:38:19 -0300

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RECURSOSPY - Menú Principal</title>
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+	<h1>RECURSOSPY</h1>
+
+	<ul class="REFERENCIALES">
+		<li><a href="DATOS/PAIS/pais.php">Datos > País</a></li>
+		<li><a href="DATOS/ESTADO/estado.php">Datos > Estado</a></li>
+		<li><a href="DATOS/CIUDAD/ciudad.php">Datos > Ciudad</a></li>
+	</ul>
+	
+	<ul class="PERFIL-DE-USUARIO">
+		<li><a href="modificar.html">Perfil > Modificar</a></li>
+		<li><a href="logout.html">Perfil > Cerrar Sesión</a></li>
+	</ul>
+</body>
+</html>
